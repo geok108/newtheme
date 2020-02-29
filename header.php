39666a5f1f8579f83eec16d276ea6d10518ac64f
George Kleanthous <george.kleanthous@hotmail.com>
2020-02-29 19:42:49 +0200

    implement structure

--- header.php
+++ header.php
@@ -2,6 +2,20 @@
 <html>
 <head>
 	<?php wp_head();?>
+
+	<link rel="stylesheet" type="text/css" href="bootstrap.min.css">
 	<title></title>
 </head>
 <body <?php body_class();?>>
+
+	<header class="sticky-top">
+		<div class="container">
+		<?php wp_nav_menu(
+
+			array(
+				'theme_location' => 'top-menu',
+				'menu_class' => 'navigation'
+			)
+		);?>
+	</div>
+	</header>
